93a67c2d14477a4e83755f71c59760fcb1d6911a
saurabh2160 <saurabhmanohar90@gmail.com>
2022-05-30 16:43:24 +0530

    orders create

--- src/Controllers/orderController.js
+++ src/Controllers/orderController.js
@@ -6,72 +6,78 @@ const { isValidRequestBody, isValidObjectId, isEmpty } = require("../Utilites/va
 
 
 //--------------------------Create Order--------------------------------------------------------//
-const createOrder = async(req,res)=>{
-try{
-    const userId = req.params.userId
-    if(!isValidObjectId(userId))
-    return res.status(400).send({ status: false, message: "Invalid userId ID" })
-    
-    const data = req.body
-    if (isValidRequestBody(data)) 
-    return res.status(400).send({ status: false, message: "Empty request body" });
-    
-    const {cartId} = data
-    if(isEmpty(cartId))return res.status(400).send({ status: false, message: "cart ID required" })
-    if(!isValidObjectId(cartId))
-    return res.status(400).send({ status: false, message: "Invalid cart ID" })
+const createOrder = async (req, res) => {
+    try {
+        const userId = req.params.userId
+        if (!isValidObjectId(userId))
+            return res.status(400).send({ status: false, message: "Invalid userId ID" })
 
-    const findUser = await userModel.findOne({ _id: userId })
-    if (!findUser) return res.status(404).send({ status: false, message: "User does not exists" })
+        const data = req.body
+        if (isValidRequestBody(data))
+            return res.status(400).send({ status: false, message: "Empty request body" });
 
-    const tokenUserId = req.decodeToken.userId;
+        const { cartId } = data
+        if (isEmpty(cartId)) return res.status(400).send({ status: false, message: "cart ID required" })
+        if (!isValidObjectId(cartId))
+            return res.status(400).send({ status: false, message: "Invalid cart ID" })
+
+        const findUser = await userModel.findOne({ _id: userId })
+        if (!findUser) return res.status(404).send({ status: false, message: "User does not exists" })
+
+        const tokenUserId = req.decodeToken.userId;
         if (tokenUserId !== findUser._id.toString())
             return res.status(403).send({ status: false, message: "Unauthorized access" });
-    
-    const findCart = await cartModel.findOne({ _id: cartId })
-    if (!findCart) return res.status(404).send({ status: false, message: "No cart found" })
-    if(findCart.items.length === 0 ) return res.status(400).send({status:false, message:"No Items in cart"})
-            
-    let totalQ = 0 
-    let cartItems = findCart.items
-    for (let i = 0; i < cartItems.length; i++) {
-        totalQ +=cartItems[i].quantity
-    }
-        
-    const orderDetails ={}
-    orderDetails['userId'] = userId
-    orderDetails['items'] =findCart.items
-    orderDetails['totalPrice'] =findCart.totalPrice
-    orderDetails['totalItems'] = findCart.items.length
-    orderDetails['totalQuantity'] =totalQ
 
-    //Change in cart model
-    findCart.items =[]
-    findCart.totalItems = 0
-    findCart.totalPrice = 0
-    
-    await findCart.save()
-    const getOrder = await orderModel.create(orderDetails)
-    
-    let obj={
-        userId: getOrder.userId,
-        items:getOrder.items,
-        totalPrice: getOrder.totalPrice,
-        totalItems: getOrder.totalItems,
-        totalQuantity:getOrder.totalQuantity,
-        cancellable: true,
-        status: "pending",
-        _id: getOrder._id,
-        createdAt: getOrder.createdAt,
-         updatedAt: getOrder.updatedAt
-    }
-    return res.status(201).send({status:true, message:"Order Placed Success", data:obj})
+        const findCart = await cartModel.findOne({ _id: cartId })
+        if (!findCart) return res.status(404).send({ status: false, message: "No cart found" })
+        if (findCart.items.length === 0) return res.status(400).send({ status: false, message: "No Items in cart" })
 
-}catch (err) {
+        let totalQ = 0
+        let cartItems = findCart.items
+        let productId
+        for (let i = 0; i < cartItems.length; i++) {
+            totalQ += cartItems[i].quantity
+            productId = cartItems[i].productId.toString();
+        }
+        let validProduct = await productModel.findOne({ _id: productId, isDeleted: true })
+        if (validProduct)
+            return res.status(404).send({ status: false, message: `${validProduct.title} Product in your cart has been deleted` })
+
+        const orderDetails = {}
+        orderDetails['userId'] = userId
+        orderDetails['items'] = findCart.items
+        orderDetails['totalPrice'] = findCart.totalPrice
+        orderDetails['totalItems'] = findCart.items.length
+        orderDetails['totalQuantity'] = totalQ
+
+        //Change in cart model
+        findCart.items = []
+        findCart.totalItems = 0
+        findCart.totalPrice = 0
+
+        await findCart.save()
+        const getOrder = await orderModel.create(orderDetails)
+        if(!getOrder)return res.status(400).send({ status: true, message: "Order not Placed" })
+
+        let obj = {
+            userId: getOrder.userId,
+            items: getOrder.items,
+            totalPrice: getOrder.totalPrice,
+            totalItems: getOrder.totalItems,
+            totalQuantity: getOrder.totalQuantity,
+            cancellable: true,
+            status: "pending",
+            _id: getOrder._id,
+            createdAt: getOrder.createdAt,
+            updatedAt: getOrder.updatedAt
+        }
+        return res.status(201).send({ status: true, message: "Order Placed Success", data: obj })
+
+    } catch (err) {
         return res.status(500).send({ err: err.message });
     }
-    
+
 }
 
-module.exports={createOrder}
+module.exports = { createOrder }
 
